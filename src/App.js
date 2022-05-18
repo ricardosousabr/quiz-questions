fd3ee5b586d8e42057a107dc3c19aa3a665701ca
ricardo <ricardo.br.pi@gmail.com>
2022-05-17 22:46:17 -0300

    feat: play the quiz

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Quiz from './pages/Quiz';
+import './styles/global.css';
 
 function App() {
   return <Quiz />;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import '../styles/quiz.css';
 
 function Quiz() {
-  return <h1>Hello word</h1>;
+  return (
+    <div className="box">
+      <div>
+        <h1>Welcome to quiz for studies</h1>
+      </div>
+      <div>
+        <main className="quizBox">
+          <div>
+            <h2 className="nameQuestion">Question</h2>
+          </div>
+          <div>
+            <ul>
+              <li className="list">
+                <input type="checkbox" />
+                <p>Alternative one</p>
+              </li>
+              <li className="list">
+                <input type="checkbox" />
+                <p>Alternative two</p>
+              </li>
+              <li className="list">
+                <input type="checkbox" />
+                <p>Alternative three</p>
+              </li>
+              <li className="list">
+                <input type="checkbox" />
+                <p>Alternative four</p>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
