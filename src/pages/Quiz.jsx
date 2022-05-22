cf5dcca5bc48bc2045f175bd05f7fbbd5204f79a
ricardo <ricardo.br.pi@gmail.com>
2022-05-22 20:13:48 -0300

    feat: check if the alternative is true

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/quiz.css';
 
 function Quiz() {
+  const [question, setQuestion] = useState();
+
+  function checkQuestion() {
+    if (question) {
+      alert('Resposta correta');
+    } else {
+      alert('Resposta errada');
+    }
+  }
+
   return (
     <div className="box">
       <div>
@@ -10,27 +20,32 @@ function Quiz() {
       <div>
         <main className="quizBox">
           <div>
-            <h2 className="nameQuestion">Question</h2>
+            <h2 className="nameQuestion">Quem descobriu o brasil</h2>
           </div>
           <div>
             <ul>
               <li className="list">
-                <input type="checkbox" />
-                <p>Alternative one</p>
+                <input type="checkbox" onChange={() => setQuestion(false)} />
+                <p>Burro Jair Bolsonaro</p>
               </li>
               <li className="list">
-                <input type="checkbox" />
-                <p>Alternative two</p>
+                <input type="checkbox" onChange={() => setQuestion(true)} />
+                <p>Pedro Álvares Cabral</p>
               </li>
               <li className="list">
-                <input type="checkbox" />
-                <p>Alternative three</p>
+                <input type="checkbox" onChange={() => setQuestion(false)} />
+                <p>Ex-presidiário Lula</p>
               </li>
               <li className="list">
-                <input type="checkbox" />
-                <p>Alternative four</p>
+                <input type="checkbox" onChange={() => setQuestion(false)} />
+                <p>Cristóvão Colombo</p>
               </li>
             </ul>
+          </div>
+          <div>
+            <button type="button" onClick={checkQuestion()}>
+              Check
+            </button>
           </div>
         </main>
       </div>
